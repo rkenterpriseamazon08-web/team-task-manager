79cdbb60913671e2d7f84a006237e30c582db8ee
R.K. Enterprise <rkenterpriseamazon08@gmail.com>
2026-04-19 09:48:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics"
 const firebaseConfig = {
   apiKey: "AIzaSyDZWGaDwj3K6ofuTyBMUIjEUg0jn37iSwI",
   authDomain: "team-task-manager-chat.firebaseapp.com",
