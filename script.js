49a4cc7e4ad19c1e159ab6e31f500ce45ae221db
R.K. Enterprise <rkenterpriseamazon08@gmail.com>
2026-04-19 11:40:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
 
 const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyvsOgYah2WrM4_r_yGMlZrRgg-HAs0A9_ZGt9n5yjfyxS8yhpC4eAlEDWAElJBzFKDsQ/exec";
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyvsOgYah2WrM4_r_yGMlZrRgg-HAs0A9_ZGt9n5yjfyxS8yhpC4eAlEDWAElJBzFKDsQ/exec";
 
 // -----------------------------
 // BASIC ELEMENTS
